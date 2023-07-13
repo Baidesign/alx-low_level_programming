0ae39b643e44ea92c2a2015f5adef0c0d9c67cea
Baidesign <baikiriga01@gmail.com>
2023-07-13 13:05:21 -0700

    bettyput

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program where we use the puts function to print a line
+ * we are not allowd to use printf in this code
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
