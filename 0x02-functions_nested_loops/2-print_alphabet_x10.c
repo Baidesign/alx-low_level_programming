936052d48df1affd2d746c796341f719b32cb892
Baidesign <baikiriga01@gmail.com>
2023-07-19 14:49:03 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
+ *
+ *Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
