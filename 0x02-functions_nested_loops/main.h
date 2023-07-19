6801bb767b509ab1262a34eb0a394f88a26077da
Baidesign <baikiriga01@gmail.com>
2023-07-19 13:29:26 -0700

    putchar1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_tp-98(int n);
+void print_tp_98(int n);
 void print_times_table(int n);
 
 #endif
