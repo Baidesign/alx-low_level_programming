61eaa2b1450735cba347165b395578324e2d9d22
Baidesign <baikiriga01@gmail.com>
2023-07-25 13:16:39 -0700

    reset98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * -putchar - it write char c to standout
+ *  @c: Character to print
+ *  Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
